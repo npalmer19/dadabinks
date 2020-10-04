c6314a44de30625798e1186fc19c15be02783979
npalmer19 <68623882+npalmer19@users.noreply.github.com>
2020-10-05 10:52:33 +1100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const msalConfig = {
   auth: {
     clientId: 'c824ef69-93ed-4f9c-b717-ab57e5cf0551',
-    redirectUri: 'https://npalmer19.github.io/dadabinks/'
+    redirectUri: 'http://npalmer19.github.io/dadabinks/'
   },
   cache: {
     cacheLocation: "sessionStorage",
